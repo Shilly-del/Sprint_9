41a1d4e863b57f522265a51570117734e3e1b352
Леонид Пионер <peon-russ@mail.ru>
2026-03-21 19:49:57 +0300

    Create workflow

--- conftest.py
+++ conftest.py
@@ -2,6 +2,7 @@ import allure
 import pytest
 
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 from constants import Url
 from helper import User, Recipe
@@ -9,10 +10,15 @@ from helper import User, Recipe
 
 @pytest.fixture
 def driver():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")    
+    chrome_options.add_argument("--no-sandbox")   
+    chrome_options.add_argument("--disable-dev-shm-usage") 
+    chrome_options.add_argument("--window-size=1920,1080")
     with allure.step(f"Создаём пользователя"):
         user = User()
     with allure.step(f"Инициализируем драйвер"):
-        driver = webdriver.Chrome()
+        driver = webdriver.Chrome(options=chrome_options)
     with allure.step(f"Открываем стартовую страницу"):   
         driver.get(Url.BASE)
     yield driver, user
@@ -21,12 +27,17 @@ def driver():
 
 @pytest.fixture
 def driver_recipe():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")     
+    chrome_options.add_argument("--no-sandbox")    
+    chrome_options.add_argument("--disable-dev-shm-usage") 
+    chrome_options.add_argument("--window-size=1920,1080")
     with allure.step(f"Создаём пользователя"):
         user = User()
     with allure.step(f"Создаём данные рецепта"):
         recipe = Recipe()
     with allure.step(f"Инициализируем драйвер"):
-        driver = webdriver.Chrome()
+        driver = webdriver.Chrome(options=chrome_options)
     with allure.step(f"Открываем стартовую страницу"):   
         driver.get(Url.BASE)
     yield driver, user, recipe
